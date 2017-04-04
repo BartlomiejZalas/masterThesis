95fa2873140882cb3a9c3583e21c17e1b62ffc2b
baza <Pzkpfw3D>
2017-04-05 00:54:45 +0200

    configuration client added

---
+++ configurationserver/api/src/main/java/com/zalas/masterthesis/configurationserver/api/client/ConfigurationClient.java
@@ -0,0 +1,44 @@
+package com.zalas.masterthesis.configurationserver.api.client;
+
+import com.zalas.masterthesis.configurationserver.api.constants.ConfigurationConstants;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static com.zalas.masterthesis.configurationserver.api.constants.ConfigurationConstants.*;
+
+public class ConfigurationClient {
+
+    public static final String CONFIGURATION_ENDPOINT_URL = "http://localhost:8888/configuration";
+
+
+    public Value getConfiguration(ConfigurationConstants key) throws ConfigurationClientException {
+        WebTarget target = ClientBuilder.newClient().target(CONFIGURATION_ENDPOINT_URL).path("/get").queryParam("key", key);
+        Invocation.Builder invocationBuilder = target.request();
+        Response response = invocationBuilder.get();
+
+        return Value.valueOf(handleResponse(response));
+    }
+
+    public void setConfiguration(ConfigurationConstants key, Value value) throws ConfigurationClientException {
+        WebTarget target = ClientBuilder.newClient().target(CONFIGURATION_ENDPOINT_URL).path("/change").queryParam("key", key).queryParam("value", value);
+        Invocation.Builder invocationBuilder = target.request();
+        Response response = invocationBuilder.post(Entity.entity("", MediaType.TEXT_PLAIN));
+
+        handleResponse(response);
+    }
+
+    private String handleResponse(Response response) throws ConfigurationClientException {
+        if (response.getStatus() != 200) {
+            String message = response.readEntity(String.class);
+            throw new ConfigurationClientException(message);
+        } else {
+            return response.readEntity(String.class);
+        }
+    }
+
+}
---
+++ configurationserver/api/src/main/java/com/zalas/masterthesis/configurationserver/api/client/ConfigurationClientException.java
@@ -0,0 +1,8 @@
+package com.zalas.masterthesis.configurationserver.api.client;
+
+public class ConfigurationClientException extends Throwable {
+
+    public ConfigurationClientException(String message) {
+        super(message);
+    }
+}
