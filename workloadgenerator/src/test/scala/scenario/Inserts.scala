e56fc330c2dcd6ac58b5e4e7e247a4caf3a03c73
baza <Pzkpfw3D>
2017-04-15 19:52:41 +0200

    Insert scenario added

---
+++ workloadgenerator/src/test/scala/scenario/Inserts.scala
@@ -0,0 +1,18 @@
+package scenario
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class Inserts extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:8080/testedApplication/")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+
+  val scn = scenario("Inserts")
+    .exec(http("call").put("/add").queryParam("categoryName", "newCategory"))
+  setUp(
+    scn.inject(rampUsers(20) over(5 seconds))
+  ).protocols(httpConf)
+}
