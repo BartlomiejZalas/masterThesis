c758647a3906353ff677064e82bb4e5eaf89426d
baza <Pzkpfw3D>
2017-05-03 00:00:54 +0200

    batch tgatling extracted

--- workloadgenerator/src/test/scala/scenario/InsertsBatchTest.scala
+++ workloadgenerator/src/test/scala/scenario/InsertsBatchTest.scala
@@ -2,17 +2,22 @@ package scenario
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
-class Inserts extends Simulation {
+class InsertsBatchTest extends Simulation {
 
   val httpConf = http
     .baseURL("http://localhost:8080/testedApplication/")
     .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 
-  val scn = scenario("Inserts")
+  val scn1 = scenario("Inserts Low")
+    .exec(http("call").put("/add").queryParam("categoryName", "newCategory"))
+
+  val scn2 = scenario("Inserts Intensive").pause(60 second)
     .exec(http("call").put("/add").queryParam("categoryName", "newCategory"))
   setUp(
-    scn.inject(rampUsers(20) over(5 seconds))
+    scn1.inject(rampUsers(60) over(60 seconds)),
+    scn2.inject(rampUsers(1000) over(60 seconds))
   ).protocols(httpConf)
 }
